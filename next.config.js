f0aca2996dd28b05c20b54a8584c4c2d0ce82702
Emdadul Haque Apu <emdadul.apu@savethechildren.org>
2023-09-20 14:14:42 +0600

    css minification is prevented for devs css minification is enabled for server

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'dist',
@@ -10,36 +9,47 @@ const nextConfig = {
     domains: ['via.placeholder.com', 'api.slingacademy.com'], // Add the domain here
   },
   webpack: (
-    config
+    config,
+    server
   ) => {
-    config.optimization = {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: 'static/style',
-            type: 'css/mini-extract',
-            chunks: 'all',
-            enforce: true
+    console.log('isDev:', server.dev)
+    if (server.dev) {
+      return config
+    }
+    else {
+      const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+      const TerserPlugin = require('terser-webpack-plugin');
+      const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+      config.optimization = {
+        splitChunks: {
+          cacheGroups: {
+            styles: {
+              name: 'static/style',
+              type: 'css/mini-extract',
+              chunks: 'all',
+              enforce: true
+            }
           }
-        }
-      },
-      minimize: true,
-      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
-    };
-    config.plugins.push(new MiniCssExtractPlugin());
-    config.plugins.push(new MiniCssExtractPlugin({ filename: '[name].css', linkType: 'text/css' }));
-    config.module.rules.push({
-      test: [/\.css$/i, /\.scss$/, /\.s[ac]ss$/i],
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader
         },
-        'css-loader',
-        'sass-loader'
-      ]
-    });
+        minimize: true,
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
+      };
+      config.plugins.push(new MiniCssExtractPlugin());
+      config.plugins.push(new MiniCssExtractPlugin({ filename: '[name].css', linkType: 'text/css' }));
+      config.module.rules.push({
+        test: [/\.css$/i, /\.scss$/, /\.s[ac]ss$/i],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      });
 
-    return config;
+      return config;
+    }
+
   }
 };
 
